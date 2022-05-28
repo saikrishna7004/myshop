65ad1ccdc97c9161359b15012ab5c7fa3b731d0f
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2022-05-28 21:40:20 +0530

    Added forgot password handlers

--- pages/login.js
+++ pages/login.js
@@ -97,7 +97,7 @@ const Login = ({ setCookie, setCart, setReloadKey }) => {
                         </div>
 
                         <div className="text-sm">
-                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
+                            <a href="/forgot" className="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your password? </a>
                         </div>
                     </div>
 
