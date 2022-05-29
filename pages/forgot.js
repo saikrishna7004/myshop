e9ea9c8f189ced7adde722dc774ec487cd7c3bd9
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2022-05-29 20:23:12 +0530

    Fixed wrong id taken in uploadCart

--- pages/forgot.js
+++ pages/forgot.js
@@ -31,6 +31,7 @@ const Forgot = ({ recaptcha, serverUrl }) => {
 
 	const resetPass = async (e)=>{
 		e.preventDefault()
+		console.log(password, cpassword)
 		if(password!=cpassword){
 			toast.error("Password and Confirm Password doesn't match")
 			return
