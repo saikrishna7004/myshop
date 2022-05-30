e512737bdf3f7f432e2914dbbbdf356a6ca6c980
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2022-05-30 08:18:18 +0530

    Fixed minor design issues

--- pages/forgot.js
+++ pages/forgot.js
@@ -71,7 +71,7 @@ const Forgot = ({ recaptcha, serverUrl }) => {
 						<div className="rounded-md shadow-sm -space-y-px">
 							<div>
 								<label htmlFor="password" className="sr-only">Password</label>
-								<input id="password" name="password" type="password" onChange={updateHandler} value={data.password} autoComplete="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
+								<input id="password" name="password" type="password" onChange={updateHandler} value={data.password} autoComplete="password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" />
 							</div>
 							<div>
 								<label htmlFor="cpassword" className="sr-only">Confirm Password</label>
@@ -142,7 +142,7 @@ const Forgot = ({ recaptcha, serverUrl }) => {
                     <div className="rounded-md shadow-sm -space-y-px">
                         <div className='mb-2'>
                             <label htmlFor="email" className="sr-only">Email</label>
-                            <input id="email" name="email" type="text" onChange={updateHandlerEmail} value={email} required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email" />
+                            <input id="email" name="email" type="text" onChange={updateHandlerEmail} value={email} required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email" />
                         </div>
 						<div className='text-sm text-gray-600'>Don't worry, we don't share your email with anyone</div>
                     </div>
