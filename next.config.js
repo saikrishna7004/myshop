38a3d18bf092c07aeef5041d4b0ad9ae1ccc5013
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2022-07-12 22:44:14 +0530

    Added PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache')
 
 const nextConfig = withPWA({
 	reactStrictMode: true,
@@ -15,7 +16,7 @@ const nextConfig = withPWA({
 	},
 	pwa: {
 		dest: 'public',
-		disable: process.env.NODE_ENV === 'development',
+		runtimeCaching,
 	},
 	reactStrictMode: true
 })
