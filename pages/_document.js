531543724fc761cdc54cd0f8d14c7437e1b398dc
saikrishna7004 <74298585+saikrishna7004@users.noreply.github.com>
2022-07-13 11:41:06 +0530

    Updated img with next/image

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from "next/script";
 
 export default function Document() {
     return (
@@ -8,7 +9,7 @@ export default function Document() {
                 <link rel='manifest' href='/manifest.json' />
                 <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png"/>
                 <meta name="theme-color" content="#000000"/>
-                <script src="register-sw.js"></script>
+                <Script src="register-sw.js"></Script>
             </Head>
             <body>
                 <Main />
